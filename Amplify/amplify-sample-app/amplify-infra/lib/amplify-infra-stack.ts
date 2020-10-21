b54f3734d9bed7bc207429de373db59783117e30
Saeid Pahlevan <s.pahlevan@fugro.com>
2020-10-21 09:34:39 +0800

    Start of the repo

---
+++ Amplify/amplify-sample-app/amplify-infra/lib/amplify-infra-stack.ts
@@ -0,0 +1,27 @@
+import * as cdk from '@aws-cdk/core';
+import * as codecommit from '@aws-cdk/aws-codecommit';
+import * as amplify from '@aws-cdk/aws-amplify';
+
+export class AmplifyInfraStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const amplifyReactSampleRepo = new codecommit.Repository(
+      this,
+      'AmplifyReactTestRepo',
+      {
+        repositoryName: 'amplify-react-test-repo',
+        description:
+          'CodeCommit repository that will be used as the source repository for the sample react app and the cdk app',
+      }
+    );
+
+    const amplifyApp = new amplify.App(this, 'sample-react-app', {
+      sourceCodeProvider: new amplify.CodeCommitSourceCodeProvider({
+        repository: amplifyReactSampleRepo,
+      }),
+    });
+    const masterBranch = amplifyApp.addBranch('master');
+  }
+}
