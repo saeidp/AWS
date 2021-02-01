0f6b959583110aaea9ec1352579800eab7ddd512
Saeid Pahlevan <s.pahlevan@fugro.com>
2021-02-01 18:28:13 +0800

    amplify apigateway fargate app

---
+++ Amplify/APIGateway-Fargate-app/serverless-containers/amplify/backend/api/containerdb8379df/src/lib/app.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+// Enable CORS for all the methods
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+app.get('/', (req, res) => {
+    res.send('Hello from container!');
+});
+app.listen(80, () => {
+    console.log('listening on port 80');
+});
+//# sourceMappingURL=app.js.map
