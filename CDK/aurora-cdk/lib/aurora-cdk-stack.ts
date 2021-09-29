07e1d2e220d23047f70ff454c2f28b3fb0b70cf0
saeid <saeidpahlevan@hotmail.com>
2021-09-29 11:29:08 +0800

    cdk: add aurora RDS

--- CDK/aurora-cdk/lib/aurora-cdk-stack.ts
+++ CDK/aurora-cdk/lib/aurora-cdk-stack.ts
@@ -3,60 +3,94 @@ import * as rds from '@aws-cdk/aws-rds';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as kms from '@aws-cdk/aws-kms';
 
-
 export class AuroraCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const vpcId = "vpc-0e787a0ed74ba0420";
+    const ingressRuleDescription = "systems-subnet-postgres";
+    const sgDescription = "Security Group for other accounts to open up ingress to the data platform";
+
+
+    const vpcId = this.node.tryGetContext('vpcid');
+    const cidr = this.node.tryGetContext('cidr');
+    const securityGroupName = this.node.tryGetContext('securitygroupname');
+    const clusterIdentifier = this.node.tryGetContext('clusteridentifier');
+    const databaseName = this.node.tryGetContext('databasename');
+    const username = this.node.tryGetContext('username');
+    const kmsAlias = this.node.tryGetContext('kmsalias');
 
     const vpc = ec2.Vpc.fromLookup(this, "VPC", {
       isDefault: false,
-      vpcId: vpcId
+      vpcId: vpcId,
     });
 
-    const kmsKey = kms.Alias.fromAliasName(this, 'kmsKey', 'alias/aws/rds');
+    const sg = new ec2.SecurityGroup(this, "securityGroup", {
+      vpc: vpc,
+      securityGroupName: securityGroupName,
+      allowAllOutbound: true,
+      description: sgDescription
+    });
 
-    const cluster = new rds.DatabaseCluster(this, 'data-platform-ods-dev02', {
-      clusterIdentifier: "data-platform-ods-dev02",
+
+    sg.connections.allowFrom(new ec2.Connections({
+      securityGroups: [sg]
+    }),
+      ec2.Port.allTraffic(),
+      '-'
+    )
+
+    sg.addIngressRule(ec2.Peer.ipv4(cidr), ec2.Port.tcp(5432), ingressRuleDescription);
+
+    const kmsKey = kms.Alias.fromAliasName(this, 'kmsKey', kmsAlias);
+
+    const cluster = new rds.DatabaseCluster(this, "cluster", {
+      clusterIdentifier: clusterIdentifier,
       engine: rds.DatabaseClusterEngine.auroraPostgres({ version: rds.AuroraPostgresEngineVersion.VER_13_3 }),
-
-      credentials: rds.Credentials.fromGeneratedSecret('dataplatform', {
-        secretName: "dataplatform"
-      }), // Optional - will default to 'admin' username and generated password
-      defaultDatabaseName: "data_platform_ods_dev02",
+      credentials: rds.Credentials.fromGeneratedSecret(username, {
+        secretName: username
+      }),
+      defaultDatabaseName: databaseName,
       cloudwatchLogsExports: ['postgresql'],
-
       instances: 1,
       storageEncrypted: true,
       storageEncryptionKey: kmsKey,
       iamAuthentication: true,
       instanceProps: {
-
-        // optional , defaults to t3.medium
-        // instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.SMALL),
         instanceType: ec2.InstanceType.of(ec2.InstanceClass.R6G, ec2.InstanceSize.LARGE),
         vpcSubnets: {
           subnetType: ec2.SubnetType.PRIVATE,
         },
         vpc,
+        securityGroups: [sg]
       },
     });
+  }
+}
 
-    // const dbInstance = new rds.DatabaseInstance(this, "db-instance", {
-    //   vpc: vpc,
+    //const vpcId = "vpc-0e787a0ed74ba0420";
 
+    //const clusterId = "data-platform-ods-dev01";
+    // const securityGroupId = "dataplatform-ingress";
+    //const secreteUserName = "dataplatform";
+    //const defaultDatabaseName =  "data_platform_ods_dev01";
+
+
+    // const clusterIdentifier = new cdk.CfnParameter(this, 'clusterIdentifier', {
+    //   type: 'String',
+    //   default: "dataplatform-ods-dev01",
+    // });
+    // const defaultDatabaseName = new cdk.CfnParameter(this, 'defaultDatabaseName', {
+    //   type: 'String',
+    //   default: "dataplatform_ods_dev01",
+    // });
+
+    // const securityGroupName = new cdk.CfnParameter(this, 'securityGroupName', {
+    //   type: 'String',
+    //   default: "dataplatform-ingress",
+    // });
+
+    // const secretUserName = new cdk.CfnParameter(this, 'secreteUserName', {
+    //   type: 'String',
+    //   default: "dataplatform",
     // });
 
 
-    // const subnets = vpc.selectSubnets({
-    //   availabilityZones: ["ap-southeast-2a", "ap-southeast-2b", "app-southeast-2c"]
-    // })
-
-    //const subnets= vpc.privateSubnets;
-
-
-    //const dbInstance = new rds.DatabaseInstance(this, 'db-instance', )
-
-
-  }
-}
