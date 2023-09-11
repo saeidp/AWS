3001ffd33c5a83a292a7cf0b29412d30c53ae4b6
Saeid Pahlevan <saeid.pahlevan@curtin.edu.au>
2023-09-11 18:38:22 +0800

    custom resource

---
+++ CDK/custom-resource/test/custom-resource.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CustomResource from '../lib/custom-resource-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/custom-resource-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CustomResource.CustomResourceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
