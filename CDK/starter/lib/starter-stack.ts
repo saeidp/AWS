ba6221962bb6eb03560952c56e495ac9b2108580
Saeid Pahlevan <saeid.pahlevan@curtin.edu.au>
2023-10-03 07:26:27 +0800

    cdk add ec2

--- CDK/starter/lib/starter-stack.ts
+++ CDK/starter/lib/starter-stack.ts
@@ -1,11 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { VPC } from '../construct/vpc/vpc';
+import { EC2 } from '../construct/ec2/ec2'
 
 export class StarterStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new VPC(this, 'my-cdk-vpc');
+    // const vpc = new VPC(this, 'my-cdk-vpc');
+    const ec2 = new EC2(this, 'my-cdk-ec2');
 
   }
 }
