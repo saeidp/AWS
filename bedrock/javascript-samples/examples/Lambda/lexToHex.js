0af35b60d29242e9f94883fd16ff01eec0749272
saeid pahlevan <saeid.pahlevan@curtin.edu.au>
2024-10-10 16:21:41 +0800

    bedrock: lex to llm lambda

--- bedrock/javascript-samples/examples/Lambda/lexToHex.js
+++ bedrock/javascript-samples/examples/Lambda/lexToHex.js
@@ -1,9 +1,6 @@
-// Import the required AWS SDK client for Bedrock
 import { BedrockRuntimeClient, InvokeModelCommand } from "@aws-sdk/client-bedrock-runtime";
 
-// Initialize the Bedrock client
 const client = new BedrockRuntimeClient({ region: 'ap-southeast-2' });  // Adjust to your region
-
 async function invokeClaude3Haiku(prompt) {
     const command = new InvokeModelCommand({
         modelId: 'anthropic.claude-3-haiku-20240307-v1:0',  // Model ID for Claude 3 Haiku
@@ -11,7 +8,7 @@ async function invokeClaude3Haiku(prompt) {
         accept: 'application/json',
         body: JSON.stringify({
             "anthropic_version": "bedrock-2023-05-31",
-            "max_tokens": 1000,
+            "max_tokens": 100,
             "messages": [
                 {
                     "role": "user",
@@ -38,8 +35,37 @@ async function invokeClaude3Haiku(prompt) {
     }
 }
 
-// Invoke the function
-const result = await invokeClaude3Haiku("what is ebs in aws");
+export const handler = async (event) => {
+    console.log(event);
+    // console.log(event["inputTranscript"])
+    
+    // event["inputTranscript"]
+    const result = await invokeClaude3Haiku(event["inputTranscript"]);
+    console.log(result["content"][0]["text"]);
+    
+    const custom_response = {
+        messages: [
+            {
+                "content": result["content"][0]["text"],
+                "contentType": "PlainText"
+            }
+        ],
+        requestAttributes: {},
+        sessionState: {
+            "sessionAttributes": {},
+            "dialogAction": {
+              "type": "Close",  // This indicates Lex should close the conversation after sending the message
+              "fulfillmentState": "Fulfilled"  // Set the fulfillment state
+            },        
+            "intent": {
+                "name": "285833d-intent",
+                "slots": {},
+                "state": "Fulfilled",
+                "confirmationState": "None"
+            }
+        }
+    };
 
-console.log(result["content"][0]["text"]);
-console.log(result);
+    return custom_response;
+};
+    
