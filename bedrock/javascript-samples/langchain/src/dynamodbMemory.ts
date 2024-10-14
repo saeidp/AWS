e3443bf559d9bf7ad42b8458b036ad53d09f710c
Saeid Pahlevan <saeid.pahlevan@curtin.edu.au>
2024-10-14 15:29:52 +0800

    bedrock: buffer memory

--- bedrock/javascript-samples/langchain/src/dynamodbMemory.ts
+++ bedrock/javascript-samples/langchain/src/dynamodbMemory.ts
@@ -1,11 +1,26 @@
 import { BufferMemory } from "langchain/memory";
 import { DynamoDBChatMessageHistory } from "@langchain/community/stores/message/dynamodb";
-import { ChatOpenAI } from "@langchain/openai";
 import { ConversationChain } from "langchain/chains";
+import {ChatBedrockConverse} from "@langchain/aws"; 
+
+
+const llm = new ChatBedrockConverse({
+  model: "anthropic.claude-3-haiku-20240307-v1:0",
+  region: "ap-southeast-2",
+  maxTokens: 300,
+  temperature: 0.1,
+  topP: 0.9
+  // region: process.env.BEDROCK_AWS_REGION,
+  // credentials: {
+  //   accessKeyId: process.env.BEDROCK_AWS_ACCESS_KEY_ID!,
+  //   secretAccessKey: process.env.BEDROCK_AWS_SECRET_ACCESS_KEY!,
+  // },
+});
+
 
 const memory = new BufferMemory({
     chatHistory: new DynamoDBChatMessageHistory({
-        tableName: "langchain",
+        tableName: "285833d_langchain",
         partitionKey: "id",
         sessionId: new Date().toISOString(), // Or some other unique identifier for the conversation
         // config: {
@@ -18,7 +33,7 @@ const memory = new BufferMemory({
     }),
 });
 
-const model = new ChatOpenAI();
+const model = llm
 const chain = new ConversationChain({ llm: model, memory });
 async function main() {
     const res1 = await chain.invoke({ input: "Hi! I'm Jim." });
